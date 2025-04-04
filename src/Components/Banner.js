260c5c1f7cee98aeeee2ebcde92dbe617f42af67
codewithafzall <afzalshaikh19992@gmail.com>
2025-04-04 15:47:52 +0530

    Carousel added

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,25 +1,85 @@
-import React from 'react';
-import Hero from"../assets/Hero.png";
-import Audit from "../assets/Audit.png";
-import ComingSoonOverlay from './ComingSoon';
+import React, { useState, useEffect } from 'react';
+import Slider from "react-slick";
+import Banner1 from "../assets/Banner1.png";
+import Banner2 from "../assets/Banner2.png";
+import Banner3 from "../assets/Banner3.png";
+import MobileBanner1 from "../assets/Banner1mobile.png";
+import MobileBanner2 from "../assets/Banner2mobile.png";
+import MobileBanner3 from "../assets/Banner3mobile.png";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 export const Banner = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const CustomPrevArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <div
+        className="absolute left-2 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer text-white bg-black/50 p-2 rounded-full"
+        onClick={onClick}
+      >
+       {"<"}
+      </div>
+    );
+  };
+  
+  const CustomNextArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <div
+        className="absolute right-2 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer text-white bg-black/50 p-2 rounded-full"
+        onClick={onClick}
+      >
+       {">"}
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    // Detect screen width to set mobile state
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+    
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Set initial state based on screen size
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 5000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    waitForAnimate: false,
+    nextArrow: <CustomNextArrow/>,  // Custom next arrow
+    prevArrow: <CustomPrevArrow/>  // Custom prev arrow
+  };
+
   return (
     <>
-    <ComingSoonOverlay>
-    <div className='banner h-[51vh] sm:h-[70vh] md:h-[100vh] flex items-center justify-start'>
-          {/* <div className='md:w-3/12 ml-[12%]'>
-             <img src={Hero} className='w-full block' alt='triumph-tiger'/>
-             <div className='flex justify-between items-center w-full mt-4 gap-x-4'>
-                <button className='bg-[#00603A] text-white uppercase w-4/12 whitespace-nowrap py-2 hover:scale-110 transition-all'>enter now</button>
-                <button className='bg-white text-[#00603A]  uppercase w-8/12 px-auto py-2 hover:scale-110 transition-all'>view ongoing contests</button>
-             </div>
-          </div> */}
-    </div>
-    </ComingSoonOverlay>
-    <div className='bg-[#EAEAEA] w-full h-auto py-10 px-5 sm:px-10 md:px-0 flex items-center justify-center'>
-         <img src={Audit} className='' alt='audit-partners'/>
-    </div>
+      <Slider {...settings} className='w-full overflow-hidden bg-[#eaeaea]'>
+        <div className='flex justify-center items-center'>
+          <img className='w-screen' src={isMobile ? MobileBanner1 : Banner1} alt="Banner 1" />
+        </div>
+        <div className='flex justify-center items-center'>
+          <img className='w-screen' src={isMobile ? MobileBanner2 : Banner2} alt="Banner 2" />
+        </div>
+        <div className='flex justify-center items-center'>
+          <img className='w-screen' src={isMobile ? MobileBanner3 : Banner3} alt="Banner 3" />
+        </div>
+      </Slider>
     </>
-  )
+  );
 }
